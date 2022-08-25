8d7d6e3ecab02a999e0b0590e289026fbafc03b6
Juan Cruz Cortiñas <juancruzcor@hotmail.com>
2022-08-25 12:29:08 -0300

    Creamos app, rutas y components como por ejemplo NavBar

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+// import logo from './logo.svg';
+import './App.css';
+import {BrowserRouter,Routes,Route} from 'react-router-dom'
+
+function App() {
+  return (
+    <BrowserRouter>
+      <div className="App">
+        <header className="App-header">
+          <img  className="App-logo" alt="logo" />
+       
+        <Routes>
+          <Route></Route>
+          <Route></Route>
+          <Route></Route>
+        </Routes>
+        </header>
+      </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ src/components/navBar/NavBar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './navbar.css'
+
+
+function NavBar() {
+  return (
+    <div>
+
+    </div>
+  )
+}
+
+export default NavBar
