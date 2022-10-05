fce2e93bc322862f9716ae977e2d0e6b3bac4965
Juan Cruz Cortiñas <juancruzcor@hotmail.com>
2022-10-05 15:12:12 -0300

    Terminamos de completar de info el ProductsData.jsx

--- src/mock/ProductsData.jsx
+++ src/mock/ProductsData.jsx
@@ -2,217 +2,218 @@ export const  productsData = [
     {
         id:1,
         name: 'TIEMPO LEGEND IX ELITE SG',
-        price: 100,
+        price: 229.99,
         stock: 5,
         category: 'Tiempo',
         img: '../../img/tiemposg.png',
-        description: 'If we talk about historical Nike boots, we have to highlight the silo time. Players like Ronaldinho, Andrea Pirlo, Francesco Totti and Carlos Tevez have used and represented them as they should be.', 
-        description2: 'High-end leather boots with mixed rubber and aluminum studs for use on damp or wet natural grass pitches. The best silo quality.'
+        description: 'If we talk about Nike historical boots, we must highlight the silo Tiempo. Players such as Ronaldinho, Andrea Pirlo, Francesco Totti or Carlos Tevez have used and represented them as they should.', 
+        description2: 'High-end leather boots with mixed rubber and aluminum studs for use on wet or damp natural grass fields. The best silo quality adapted for those who play on the best football fields without any traction problem.'
 
     }
     ,
     {
         id:2,
         name: 'TIEMPO LEGEND IX ELITE FG',
-        price: 100,
+        price: 229.99,
         stock: 5,
         category: 'Tiempo',
         img: '../../img/tiempofg.png',
-        description: 'If we talk about historical Nike boots, we have to highlight the silo time. Players like Ronaldinho, Andrea Pirlo, Francesco Totti and Carlos Tevez have used and represented them as they should be.', 
-
+        description: 'If we talk about Nike historical boots, we must highlight the silo Tiempo. Players such as Ronaldinho, Andrea Pirlo, Francesco Totti or Carlos Tevez have used and represented them as they should.', 
+        description2: 'High-end leather boots like the SG but with high rubber studs only, to be used on natural grass pitches. Undoubtedly the most used by professional football players today. The best quality silo adapted for those who play on the best football fields.'
 
     }
     ,
     {
         id:3,
         name: 'TIEMPO LEGEND IX PRO FG',
-        price: 100,
+        price: 129.99,
         stock: 5,
         category: 'Tiempo',
         img: '../../img/tiempopro.png',
-        description: 'If we talk about historical Nike boots, we have to highlight the silo time. Players like Ronaldinho, Andrea Pirlo, Francesco Totti and Carlos Tevez have used and represented them as they should be.', 
-
+        description: 'If we talk about Nike historical boots, we must highlight the silo Tiempo. Players such as Ronaldinho, Andrea Pirlo, Francesco Totti or Carlos Tevez have used and represented them as they should.', 
+        description2: 'Mid-range leather boots with high heels only made of rubber, to be used on natural or artificial grass fields. They are the most used boots by semi-professional or amateur football players since ever. A more economical version with good quality technology.'
 
     }
     ,
     {
         id:4,
         name: 'TIEMPO LEGEND IX PRO TF',
-        price: 100,
+        price: 119.99,
         stock: 5,
         category: 'Tiempo',
         img: '../../img/tiempoprotf.png',
-        description: 'If we talk about historical Nike boots, we have to highlight the silo time. Players like Ronaldinho, Andrea Pirlo, Francesco Totti and Carlos Tevez have used and represented them as they should be.', 
-        description2: ''
+        description: 'If we talk about Nike historical boots, we must highlight the silo Tiempo. Players such as Ronaldinho, Andrea Pirlo, Francesco Totti or Carlos Tevez have used and represented them as they should.', 
+        description2: 'Mid-range leather boots with low rubber studs only, to be used on artificial turf fields. They are the most used boots by semi-professional or amateur football players to play the so famous fut 5 or 7. A more economical version with good quality technology.'
 
     }
     ,
     {
         id:5,
         name: 'MERCURIAL SUPERFLY IX ELITE SG',
-        price: 100,
+        price: 274.99,
         stock: 5,
         category: 'Mercurial',
         img: '../../img/superflysg.png',
-        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. In accusamus velit numquam, iure impedit delectus voluptate vitae dolor quos, animi vel, eum modi non accusantium dolorum totam! Iure, labore veniam', 
-
+        description: "Mercurial? for all those who love football, when they see these boots, it is impossible not to think of Cristiano Ronaldo for the older ones, or Kylian Mbappé nowadays. Fast and skillful players.", 
+        description2: 'High-end leather boots with mixed rubber and aluminum studs for use on wet or damp natural grass fields. The best silo quality adapted for those who play on the best football fields without any traction problem.'
 
     }
     ,
     {
         id:6,
         name: 'MERCURIAL SUPERFLY IX ELITE FG',
-        price: 100,
+        price: 274.99,
         stock: 5,
         category: 'Mercurial',
         img: '../../img/superflyfg.png',
-        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. In accusamus velit numquam, iure impedit delectus voluptate vitae dolor quos, animi vel, eum modi non accusantium dolorum totam! Iure, labore veniam', 
-
+        description: "Mercurial? for all those who love football, when they see these boots, it is impossible not to think of Cristiano Ronaldo for the older ones, or Kylian Mbappé nowadays. Fast and skillful players.", 
+        description2: 'High-end leather boots like the SG but with high rubber studs only, to be used on natural grass pitches. Undoubtedly the most used by professional football players today. The best quality silo adapted for those who play on the best football fields.'
 
     }
     ,
     {
         id:7,
         name: 'MERCURIAL SUPERFLY IX PRO FG',
-        price: 100,
+        price: 149.99,
         stock: 5,
         category: 'Mercurial',
         img: '../../img/superflypro.png',
-        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. In accusamus velit numquam, iure impedit delectus voluptate vitae dolor quos, animi vel, eum modi non accusantium dolorum totam! Iure, labore veniam', 
-
+        description: "Mercurial? for all those who love football, when they see these boots, it is impossible not to think of Cristiano Ronaldo for the older ones, or Kylian Mbappé nowadays. Fast and skillful players.", 
+        description2: 'Mid-range leather boots with high heels only made of rubber, to be used on natural or artificial grass fields. They are the most used boots by semi-professional or amateur football players since ever. A more economical version with good quality technology.'
 
     }
     ,
     {
         id:8,
         name: 'MERCURIAL VAPOR XV ELITE SG',
-        price: 100,
+        price: 249.99,
         stock: 5,
         category: 'Mercurial',
         img: '../../img/vaporsg.png',
-        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. In accusamus velit numquam, iure impedit delectus voluptate vitae dolor quos, animi vel, eum modi non accusantium dolorum totam! Iure, labore veniam', 
-
-
+        description: "The Mercurial Vapor boots are also used by iconic players such as Robert Lewandowski and Erling Haaland, two world-class strikers who never cease to amaze us when it comes to scoring goals season after season.", 
+        description2: 'High-end leather boots with mixed rubber and aluminum studs for use on wet or damp natural grass fields. The best silo quality adapted for those who play on the best football fields without any traction problem.'
+        
     }
     ,
 
     {
         id:9,
         name: 'MERCURIAL VAPOR XV ELITE FG',
-        price: 100,
+        price: 249.99,
         stock: 5,
         category: 'Mercurial',
         img: '../../img/vaporfg.png',
-        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. In accusamus velit numquam, iure impedit delectus voluptate vitae dolor quos, animi vel, eum modi non accusantium dolorum totam! Iure, labore veniam', 
-
+        description: "The Mercurial Vapor boots are also used by iconic players such as Robert Lewandowski and Erling Haaland, two world-class strikers who never cease to amaze us when it comes to scoring goals season after season.", 
+        description2: 'High-end leather boots like the SG but with high rubber studs only, to be used on natural grass pitches. Undoubtedly the most used by professional football players today. The best quality silo adapted for those who play on the best football fields.'
 
     }
     ,
     {
         id:10,
         name: 'MERCURIAL VAPOR XV PRO FG',
-        price: 100,
+        price: 139.99,
         stock: 5,
         category: 'Mercurial',
         img: '../../img/vaporpro.png',
-        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. In accusamus velit numquam, iure impedit delectus voluptate vitae dolor quos, animi vel, eum modi non accusantium dolorum totam! Iure, labore veniam', 
-
+        description: "The Mercurial Vapor boots are also used by iconic players such as Robert Lewandowski and Erling Haaland, two world-class strikers who never cease to amaze us when it comes to scoring goals season after season.", 
+        description2: 'Mid-range leather boots with high heels only made of rubber, to be used on natural or artificial grass fields. They are the most used boots by semi-professional or amateur football players since ever. A more economical version with good quality technology.'
 
     }
     ,
     {
         id:11,
         name: 'MERCURIAL VAPOR XV PRO TF',
-        price: 100,
+        price: 119.99,
         stock: 5,
         category: 'Mercurial',
         img: '../../img/vaporprotf.png',
-        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. In accusamus velit numquam, iure impedit delectus voluptate vitae dolor quos, animi vel, eum modi non accusantium dolorum totam! Iure, labore veniam', 
-
+        description: "The Mercurial Vapor boots are also used by iconic players such as Robert Lewandowski and Erling Haaland, two world-class strikers who never cease to amaze us when it comes to scoring goals season after season.", 
+        description2: 'Mid-range leather boots with low rubber studs only, to be used on artificial turf fields. They are the most used boots by semi-professional or amateur football players to play the so famous fut 5 or 7. A more economical version with good quality technology.'
 
     }
     ,
     {
         id:12,
         name: 'PHANTOM GT II ELITE SG',
-        price: 100,
+        price: 249.99,
         stock: 5,
         category: 'Phantom',
         img: '../../img/phantomsg.png',
-        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. In accusamus velit numquam, iure impedit delectus voluptate vitae dolor quos, animi vel, eum modi non accusantium dolorum totam! Iure, labore veniam', 
-
+        description: "The most representative player is undoubtedly the Belgian Kevin De Bruyne, an extraordinary player. On the other hand, Phil Foden is another of the successful young talents who use these boots.", 
+        description2: 'High-end leather boots with mixed rubber and aluminum studs for use on wet or damp natural grass fields. The best silo quality adapted for those who play on the best football fields without any traction problem.'
 
     }
     ,
     {
         id:13,
         name: 'PHANTOM GT II ELITE FG',
-        price: 100,
+        price: 249.99,
         stock: 5,
         category: 'Phantom',
         img: '../../img/phantomfg.png',
-        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. In accusamus velit numquam, iure impedit delectus voluptate vitae dolor quos, animi vel, eum modi non accusantium dolorum totam! Iure, labore veniam', 
-
-
+        description: "The most representative player is undoubtedly the Belgian Kevin De Bruyne, an extraordinary player. On the other hand, Phil Foden is another of the successful young talents who use these boots.", 
+        description2: 'High-end leather boots like the SG but with high rubber studs only, to be used on natural grass pitches. Undoubtedly the most used by professional football players today. The best quality silo adapted for those who play on the best football fields.'
+        
     }
     ,
     {
         id:14,
         name: 'PHANTOM GT II PRO FG',
-        price: 100,
+        price: 139.99,
         stock: 5,
         category: 'Phantom',
         img: '../../img/phantompro.png',
-        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. In accusamus velit numquam, iure impedit delectus voluptate vitae dolor quos, animi vel, eum modi non accusantium dolorum totam! Iure, labore veniam', 
-
+        description: "The most representative player is undoubtedly the Belgian Kevin De Bruyne, an extraordinary player. On the other hand, Phil Foden is another of the successful young talents who use these boots.", 
+        description2: 'Mid-range leather boots with high heels only made of rubber, to be used on natural or artificial grass fields. They are the most used boots by semi-professional or amateur football players since ever. A more economical version with good quality technology.'
 
     }
     ,
     {
         id:15,
         name: 'PHANTOM GT II PRO TF',
-        price: 100,
+        price: 119.99,
         stock: 5,
         category: 'Phantom',
         img: '../../img/phantomprotf.png',
-        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. In accusamus velit numquam, iure impedit delectus voluptate vitae dolor quos, animi vel, eum modi non accusantium dolorum totam! Iure, labore veniam', 
-
+        description: "The most representative player is undoubtedly the Belgian Kevin De Bruyne, an extraordinary player. On the other hand, Phil Foden is another of the successful young talents who use these boots.", 
+        description2: 'Mid-range leather boots with low rubber studs only, to be used on artificial turf fields. They are the most used boots by semi-professional or amateur football players to play the so famous fut 5 or 7. A more economical version with good quality technology.'
 
     }
     ,
     {
         id:16,
         name: 'THE PREMIER III SG',
-        price: 100,
+        price: 109.99,
         stock: 5,
         category: 'Premier',
         img: '../../img/premiersg.png',
-        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. In accusamus velit numquam, iure impedit delectus voluptate vitae dolor quos, animi vel, eum modi non accusantium dolorum totam! Iure, labore veniam', 
-
+        description: "This silo may not be the most representative in professional football players because they are a more economical alternative to the Tiempo silo. However, players like Hector Bellerin have used them.", 
+        description2: "Mid-range leather boots with mixed rubber and aluminum studs for use on wet or damp natural grass pitches. Inspired by the Tiempo silo, The Premier is a more economical option than the other silos, but of no lesser quality. Who doesn't think of players like Ronaldinho when they see these boots?"
 
     }
     ,
     {
         id:17,
         name: 'THE PREMIER III FG',
-        price: 100,
+        price: 109.99,
         stock: 5,
         category: 'Premier',
         img: '../../img/premierfg.png',
-        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. In accusamus velit numquam, iure impedit delectus voluptate vitae dolor quos, animi vel, eum modi non accusantium dolorum totam! Iure, labore veniam', 
-
+        description: "This silo may not be the most representative in professional football players because they are a more economical alternative to the Tiempo silo. However, players like Hector Bellerin have used them.", 
+        description2: "Mid-range leather boots with high rubber studs only, to be used on natural or artificial grass pitches. Inspired by the Tiempo silo, The Premier is a more economical option than the other silos, but not of a lower quality. Who doesn't think of players like Ronaldinho when they see these boots?"
+        
 
     }
     ,
     {
         id:18,
         name: 'THE PREMIER III TF',
-        price: 100,
+        price: 94.99,
         stock: 5,
         category: 'Premier',
         img: '../../img/premierprotf.png',
-        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. In accusamus velit numquam, iure impedit delectus voluptate vitae dolor quos, animi vel, eum modi non accusantium dolorum totam! Iure, labore veniam', 
-
+        description: "This silo may not be the most representative in professional football players because they are a more economical alternative to the Tiempo silo. However, players like Hector Bellerin have used them.", 
+        description2: "Mid-range leather boots with low rubber studs only, to be used on artificial turf fields. They are the most used boots by semi-professional or amateur football players to play the so famous fut 5 or 7. A more economical version with good quality technology. Who doesn't think of players like Ronaldinho when they see these boots?"
 
     }
     ,
